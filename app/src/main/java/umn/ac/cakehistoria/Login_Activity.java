eeff7993a6383328ea19110199bede21dd16e835
vitojuliano <vito.juliano@student.umn.ac.id>
2020-04-08 21:33:18 +0700

    Update Vito - Login Activity & Signup Activity (Intent)

--- app/src/main/java/umn/ac/cakehistoria/Login_Activity.java
+++ app/src/main/java/umn/ac/cakehistoria/Login_Activity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -14,7 +15,18 @@ public class Login_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Ini Vito Juliano.
+        TextView txt_signup = findViewById(R.id.txt_SignUp);
+        txt_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent_to_signup_activity = new Intent(Login_Activity.this, SignUp_Activity.class);
+                startActivity(intent_to_signup_activity);
+
+            }
+        });
+
+
 
     }
 }
