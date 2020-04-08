2e524ccc4186377a8001b8631882e9c1166d3126
IndraSujadi <indrasujadi0@gmail.com>
2020-04-08 17:14:22 +0700

    Splash_screen&Login

---
+++ app/src/main/java/umn/ac/cakehistoria/Login_Activity.java
@@ -0,0 +1,28 @@
+package umn.ac.cakehistoria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class Login_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ImageButton google =findViewById(R.id.LoginGoogle);
+        google.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/umn/ac/cakehistoria/Splach_Screen.java
@@ -0,0 +1,32 @@
+package umn.ac.cakehistoria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+public class Splach_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach__screen);
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(Splach_Screen.this, MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+
+    }
+}
