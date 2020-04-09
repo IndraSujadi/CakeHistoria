daf187a3c24d5b44b34914221427dd7761475583
IndraSujadi <indrasujadi0@gmail.com>
2020-04-09 22:06:10 +0700

    TOP_NAV
    blm sempurna. komputer ku sudah berteriak masih ada itu kurangnya yg ungu ungu di atas itu

---
+++ app/src/main/java/umn/ac/cakehistoria/Frag_Home.java
@@ -0,0 +1,22 @@
+package umn.ac.cakehistoria;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Frag_Home extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_frag__home, container, false);
+        // coding sini
+        return view;
+    }
+}
