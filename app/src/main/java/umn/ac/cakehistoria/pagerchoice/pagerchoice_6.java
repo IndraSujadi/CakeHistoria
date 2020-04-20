50f234bcb86b7b10772a50b395f07a5c96b94ed2
Hannatassja <Hannatassja@student.umn.ac.id>
2020-04-20 10:53:45 +0700

    Create swipe for pager choice and layout page 1

--- app/src/main/java/umn/ac/cakehistoria/pagerchoice/pagerchoice_6.java
+++ app/src/main/java/umn/ac/cakehistoria/pagerchoice/pagerchoice_6.java
@@ -1,4 +1,22 @@
 package umn.ac.cakehistoria.pagerchoice;
 
-public class pagerchoice_6 {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import umn.ac.cakehistoria.R;
+
+public class pagerchoice_6 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.pagerchoice_6, container, false);
+
+        return view;
+    }
 }
---
+++ app/src/main/java/umn/ac/cakehistoria/pagerchoice/pagerchoice_adapter.java
@@ -0,0 +1,47 @@
+package umn.ac.cakehistoria.pagerchoice;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import umn.ac.cakehistoria.PagerView_1;
+import umn.ac.cakehistoria.PagerView_2;
+
+public class pagerchoice_adapter extends FragmentPagerAdapter {
+    private static int NUM_ITEMS = 7;
+
+    public pagerchoice_adapter(FragmentManager fragmentManager) {
+        super(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    // Returns total number of pages.
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    // Returns the fragment to display for a particular page.
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new pagerchoice_1();
+            case 1:
+                return new pagerchoice_2();
+            case 2:
+                return new pagerchoice_3();
+            case 3:
+                return new pagerchoice_4();
+            case 4:
+                return new pagerchoice_5();
+            case 5:
+                return new pagerchoice_5();
+            case 6:
+                return new pagerchoice_6();
+            case 7:
+                return new pagerchoice_7();
+            default:
+                return null;
+        }
+    }
+}
