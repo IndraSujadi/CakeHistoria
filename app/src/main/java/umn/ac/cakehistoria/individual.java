9b64f3f48d5b19b5e2d5a94b38008f2ed19b56c0
IndraSujadi <indrasujadi0@gmail.com>
2020-05-01 23:59:54 +0700

    individual
    java class : individual
    layout : activity_individual

---
+++ app/src/main/java/umn/ac/cakehistoria/individual.java
@@ -0,0 +1,31 @@
+package umn.ac.cakehistoria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class individual extends AppCompatActivity {
+
+    ImageView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_individual);
+
+        back = findViewById(R.id.imgBackNEW);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+}
