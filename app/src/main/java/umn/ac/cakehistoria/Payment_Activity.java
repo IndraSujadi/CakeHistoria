d006d9e917fe4072a7fadd4e14aa591c8efae3b9
Hannatassja <Hannatassja@student.umn.ac.id>
2020-05-02 13:46:12 +0700

    Payment API

--- app/src/main/java/umn/ac/cakehistoria/Payment_Activity.java
+++ app/src/main/java/umn/ac/cakehistoria/Payment_Activity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -19,7 +20,26 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.Map;
 
-public class Payment_Activity extends AppCompatActivity {
+import com.midtrans.sdk.corekit.BuildConfig;
+import com.midtrans.sdk.corekit.callback.TransactionFinishedCallback;
+import com.midtrans.sdk.corekit.core.MidtransSDK;
+import com.midtrans.sdk.corekit.core.TransactionRequest;
+import com.midtrans.sdk.corekit.core.themes.CustomColorTheme;
+import com.midtrans.sdk.corekit.models.BankType;
+import com.midtrans.sdk.corekit.models.ItemDetails;
+import com.midtrans.sdk.corekit.models.snap.Authentication;
+import com.midtrans.sdk.corekit.models.snap.CreditCard;
+import com.midtrans.sdk.corekit.models.CustomerDetails;
+
+
+import com.midtrans.sdk.corekit.models.snap.TransactionResult;
+import com.midtrans.sdk.uikit.SdkUIFlowBuilder;
+
+
+
+import java.util.ArrayList;
+
+public class Payment_Activity extends AppCompatActivity implements TransactionFinishedCallback {
 
     // Database
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -147,5 +167,86 @@ public class Payment_Activity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.btnPay).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickPay();
+            }
+        });
+
+        makePayment();
+
     }
+
+    private void makePayment(){
+        SdkUIFlowBuilder.init()
+                .setContext(this)
+                .setMerchantBaseUrl(BuildConfig.BASE_URL)
+                .setClientKey(umn.ac.cakehistoria.BuildConfig.CLIENT_KEY)
+                .setTransactionFinishedCallback(this)
+                .enableLog(true)
+                .setColorTheme(new CustomColorTheme("#777777","#f77474" , "#3f0d0d"))
+                .buildSDK();
+    }
+
+    private void clickPay(){
+        MidtransSDK.getInstance().setTransactionRequest(transactionRequest("101",2000, 1, "susus murni"));
+        MidtransSDK.getInstance().startPaymentUiFlow(Payment_Activity.this );
+
+//        Toast.makeText(this, "Coba Click" , Toast.LENGTH_LONG).show();
+
+    }
+
+    public static CustomerDetails customerDetails(){
+        CustomerDetails cd = new CustomerDetails();
+        cd.setFirstName("YOUR_PRODUCT");
+        cd.setEmail("your_email@gmail.com");
+        cd.setPhone("your_phone");
+        return cd;
+    }
+
+    public static TransactionRequest transactionRequest(String id, double price, int qty, String name){
+        TransactionRequest request =  new TransactionRequest(System.currentTimeMillis() + " " , 2000 );
+        request.setCustomerDetails(customerDetails());
+        ItemDetails details = new ItemDetails(id, price, qty, name);
+
+        ArrayList<ItemDetails> itemDetails = new ArrayList<>();
+        itemDetails.add(details);
+        request.setItemDetails(itemDetails);
+        CreditCard creditCard = new CreditCard();
+        creditCard.setSaveCard(false);
+        creditCard.setAuthentication(Authentication.AUTH_RBA);
+        creditCard.setBank(BankType.MANDIRI);
+
+        request.setCreditCard(creditCard);
+        return request;
+    }
+
+    @Override
+    public void onTransactionFinished(TransactionResult result) {
+        if(result.getResponse() != null){
+            switch (result.getStatus()){
+                case TransactionResult.STATUS_SUCCESS:
+                    Toast.makeText(this, "Transaction Sukses " + result.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+                    break;
+                case TransactionResult.STATUS_PENDING:
+                    Toast.makeText(this, "Transaction Pending " + result.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+                    break;
+                case TransactionResult.STATUS_FAILED:
+                    Toast.makeText(this, "Transaction Failed" + result.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+                    break;
+            }
+            result.getResponse().getValidationMessages();
+        }else if(result.isTransactionCanceled()){
+            Toast.makeText(this, "Transaction Failed", Toast.LENGTH_LONG).show();
+        }else{
+            if(result.getStatus().equalsIgnoreCase((TransactionResult.STATUS_INVALID))){
+                Toast.makeText(this, "Transaction Invalid" + result.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Something Wrong", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+
 }
